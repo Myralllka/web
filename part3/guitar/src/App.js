1016f157d02155881154a84b418ffad4f1a21080
Mykola Morgunenko <Olli25010@gmail.com>
2020-05-12 01:23:22 +0300

    already have my pc back. finished hw1.

---
+++ part3/guitar/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/colors.css';
+
+import Header from './templates/header'
+import Main from './templates/main_container'
+import Footer from './templates/footer'
+function App() {
+    return (
+        <div className="App">
+            <Header>
+
+            </Header>
+            <Main>
+
+            </Main>
+            <Footer>
+
+            </Footer>
+        </div>
+    );
+}
+
+export default App;
