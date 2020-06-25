f3c35ce7acef7d3c0c62cbdb3f2e370d455e3c60
Mykola Morgunenko <Olli25010@gmail.com>
2020-06-25 18:16:21 +0300

    Updates

---
+++ precache-manifest.bc5a15ee834201501f97a089818aed28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c37c69081ca106fcfc7e0321ba9cf37",
+    "url": "/web_guitar/index.html"
+  },
+  {
+    "revision": "e97018a89340c2dd3203",
+    "url": "/web_guitar/static/css/main.9c4869e8.chunk.css"
+  },
+  {
+    "revision": "ca79af6d441c4227e751",
+    "url": "/web_guitar/static/js/2.a4bdb1b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web_guitar/static/js/2.a4bdb1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97018a89340c2dd3203",
+    "url": "/web_guitar/static/js/main.aa119e4f.chunk.js"
+  },
+  {
+    "revision": "69aae1958851d71ac829",
+    "url": "/web_guitar/static/js/runtime-main.383e991d.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/web_guitar/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/web_guitar/static/media/click2.fe0ded65.wav"
+  }
+]);
